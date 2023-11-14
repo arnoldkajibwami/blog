87bb203d00bfe8b65a69a2c89bdfbb7b549807c0
Arnold kajibwami <arnoldkajinguma8@gmail.com>
2023-11-14 15:54:47 +0200

    changess

--- Backend/app.js
+++ Backend/app.js
@@ -79,12 +79,12 @@ app.use('/api', postRoute);
 
 __dirname = path.resolve()
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+// app.get('/', (req, res) => {
+//   res.send('API is running....')
+// })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
